294786c234e23bed0ec600955706d18b7a59b089
tmsBodnar <tms.bodnar@gmail.com>
2022-05-18 19:08:48 +0200

    refactored, gives and saves the config,js

--- mmm-api.js
+++ mmm-api.js
@@ -1,36 +1,29 @@
 const express = require('express');
 const fs = require('fs')
-var util = require('util')
 const app = express()
 const port = 3000
-const config = require('/home/tms/MagicMirror/config/config.js')
+const configPath = '/home/tms/MagicMirror/config/config.js'
+app.use(express.json())
 
-const modules = config['modules'];
-console.log(modules[5]['config']['location'])
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+});
 
 app.get('/', (req, res) => {
   res.send('MMM-API welcomes you! \n Please, check the readme at www.github.com/tmsBodnar/MMM-API#readme')
 })
 
-app.get('/modules', (req, res) => {
-    res.send(modules);
+app.get('/config', (req, res) => {
+  fs.readFile(configPath, (err, data) => {
+    res.send(data);
   })
-
-app.post('/modules/:moduleIndx/:position', (req, res) => {
-    config['modules'][req.params['moduleIndx']]['position'] = req.params['position'];
-    fs.writeFileSync('/home/tms/MagicMirror/config/config.js', 'let config = ' + util.inspect(config) , 'utf-8')
-
-  //  fs.writeFile("/home/tms/MagicMirror/config/config.js", configJSON, 'utf8', function (err) {
-    //  if (err) {
-      //    console.log("An error occured while writing JSON Object to File.");
-        //  return console.log(err);
-     // }
-   
-      console.log("JSON file has been saved.");
-//  });
-  res.send(config['modules'][req.params['moduleIndx']]);
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+app.put('/config', (req, res) => {
+  let conf = req.body;
+  fs.writeFileSync(configPath, 'let config = ' + JSON.stringify(conf) , 'utf-8')
+  fs.readFile(configPath, (err, data) => {
+    res.send(data);
+  })
+  
+});
