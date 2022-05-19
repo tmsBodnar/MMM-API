d4cb1ba9e889579e89ef538bfc81ff5042b86090
tmsBodnar <tms.bodnar@gmail.com>
2022-05-19 06:48:40 +0200

    started with manager webpage

---
+++ mmm-manager/manager/src/app/navigation/navigation.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
+import { ApiService } from '../services/api.service';
+
+@Component({
+  selector: 'app-navigation',
+  templateUrl: './navigation.component.html',
+  styleUrls: ['./navigation.component.css']
+})
+export class NavigationComponent {
+
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches),
+      shareReplay()
+    );
+
+  constructor(private breakpointObserver: BreakpointObserver,
+    private apiService: ApiService) {}
+
+  onSaveClicked(){
+    console.log("save clicked");
+  }
+  onLoadClicked(){
+    let result = this.apiService.getModules();
+  }
+  onResetClicked(){
+    console.log("reset clicked");
+  }
+}
