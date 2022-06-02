0bc72abfc1cb6eed2e45e9f22d3d9b885a409549
tmsBodnar <tms.bodnar@gmail.com>
2022-06-02 06:55:51 +0200

    dragdrop with position layout, not accordion

--- mmm-manager/manager/src/app/models/Position.ts
+++ mmm-manager/manager/src/app/models/Position.ts
@@ -9,16 +9,16 @@ export class Position{
     background: string;
     modules?: Module[];
   static positions: Position[] = [
-    {'name': 'top_bar','col':0, 'row':0, 'colspan':3, 'rowspan':1, background: '#30444e'},
-    {'name':'top_left', 'col':0, 'row':1, 'colspan':1, 'rowspan':1, background: '#ff1744'},
+    {'name': 'top_bar','col':0, 'row':0, 'colspan':3, 'rowspan':1, background: '#b0bec5'},
+    {'name':'top_left', 'col':0, 'row':1, 'colspan':1, 'rowspan':1, background: '#ffb2dd'},
     {'name':'top_center', 'col':1, 'row':1, 'colspan':1, 'rowspan':1, background: '#ff80ab'},
-    {'name':'top_right', 'col':2, 'row':1, 'colspan':1, 'rowspan':1, background: '#e040fb'},
-    {'name':'upper_third', 'col':0, 'row':2, 'colspan':3, 'rowspan':1, background: '#448aff'},
-    {'name':'middle_center', 'col':0, 'row':3, 'colspan':3, 'rowspan':1, background: '#69f0ae'},
-    {'name':'lower_third', 'col':0, 'row':4, 'colspan':3, 'rowspan':1, background: '#ffff00'},
-    {'name':'bottom_left', 'col':0, 'row':5, 'colspan':1, 'rowspan':1, background: '#ffab40'},
-    {'name':'bottom_center', 'col': 1, 'row':5, 'colspan':1, 'rowspan':1, background: '#ff6f00'},
-    {'name':'bottom_right', 'col':2, 'row': 5, 'colspan':1, 'rowspan':1, background: '#ff5722'},
+    {'name':'top_right', 'col':2, 'row':1, 'colspan':1, 'rowspan':1, background: '#c94f7c'},
+    {'name':'upper_third', 'col':0, 'row':2, 'colspan':3, 'rowspan':1, background: '#76ffff'},
+    {'name':'middle_center', 'col':0, 'row':3, 'colspan':3, 'rowspan':1, background: '#18ffff'},
+    {'name':'lower_third', 'col':0, 'row':4, 'colspan':3, 'rowspan':1, background: '#00cbcc'},
+    {'name':'bottom_left', 'col':0, 'row':5, 'colspan':1, 'rowspan':1, background: '#ffd0b0'},
+    {'name':'bottom_center', 'col': 1, 'row':5, 'colspan':1, 'rowspan':1, background: '#ff9e80'},
+    {'name':'bottom_right', 'col':2, 'row': 5, 'colspan':1, 'rowspan':1, background: '#c96f53'},
     {'name':'bottom_bar', 'col':0, 'row': 6, 'colspan':3, 'rowspan':1, background: '#b0bec5'}
     ];
 }
