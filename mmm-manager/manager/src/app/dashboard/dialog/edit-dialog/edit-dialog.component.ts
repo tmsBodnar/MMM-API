1d26226b3d12b27c2cd688ba9b27e3615b96b1a8
tmsBodnar <tms.bodnar@gmail.com>
2022-06-04 08:13:57 +0200

    put processed config to an object

--- mmm-manager/manager/src/app/dashboard/dialog/edit-dialog/edit-dialog.component.ts
+++ mmm-manager/manager/src/app/dashboard/dialog/edit-dialog/edit-dialog.component.ts
@@ -12,27 +12,34 @@ export class EditDialogComponent implements OnInit {
 
   module: Module;
   config: ModuleConfig;
+  configInput: ModuleConfig = {};
+
 
   constructor(@Inject(MAT_DIALOG_DATA) public data: Module) {
     this.module = data;
     if (this.module.config !== undefined) {
-      this.processNestedConfig(this.module.config);
+      this.processNestedConfig(this.module.config, false);
     }
+    console.log(this.configInput);
    }
 
   ngOnInit(): void {
   }
-processNestedConfig(conf: ModuleConfig){;
+processNestedConfig(conf: ModuleConfig, isNested: boolean){
       Object.keys(conf).forEach((key) => {
         if(Array.isArray(conf[key as keyof ModuleConfig])){
           const array = conf[key as keyof ModuleConfig] as Array<any>;
           array.forEach(element =>{
-            this.processNestedConfig(element);
+            this.processNestedConfig(element, true);
+            this.configInput[key]=conf[key as keyof ModuleConfig];
           });
         } else if (typeof conf[key as keyof ModuleConfig] === 'object') {
-            this.processNestedConfig(conf[key as keyof ModuleConfig] as ModuleConfig);
-        } else {
-          console.log(key, conf[key as keyof ModuleConfig]);
+            this.processNestedConfig(conf[key as keyof ModuleConfig] as ModuleConfig, true);
+            this.configInput[key]=conf[key as keyof ModuleConfig];
+        }
+        if(!isNested)
+        {
+            this.configInput[key]=conf[key as keyof ModuleConfig];
         }
       });
   }
